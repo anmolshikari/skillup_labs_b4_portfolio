04720cb144b429890aa6423183bfa2a4819d3cac
anmolshikari <Anmolshikari11111@gmail.com>
2024-10-22 21:04:00 +1100

    neary done

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('scroll', () => {
+    const scrollTop = document.documentElement.scrollTop;
+
+    const mainNav = document.querySelector(".main-nav");
+    if(scrollTop > 50) {
+        mainNav.classList.add("bg-nav");
+    }
+    else {
+        mainNav.classList.remove("bg-nav");
+    }
+});
